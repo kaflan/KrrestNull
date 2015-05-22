3a232af627247d5e5ff8c8336059b64074115c52
kaflan <kaf_90@mail.ru>
2015-05-22 22:46:15 +0300

    lalla

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 window.addEventListener('load',  function(){
     console.log('load complete');
     var cell = document.querySelectorAll('.cell'), // у массіві всі наші класси целл
-        curClass,// хранить класс правильний по кліку змінюєця
+        nextClass,// хранить класс правильний по кліку фраєра змінюєця поточний клас сім матерів бабі в цицьку
         startNewGame = document.querySelector('.startNewGame'),// нова гра
         winnerMessage = document.querySelector('.winner-message');// повідомлення про перемогу
         startNewGame.addEventListener('click', function(e){// start to new game click
@@ -10,45 +10,50 @@ window.addEventListener('load',  function(){
             cell[i].classList.remove('o'); //delete class 'o' and 'x'
             cell[i].classList.remove('x');
                 console.log('finish');
+            nextClass = '';
             }
     });
     for(var i = 0; i < cell.length; i++){ //шурує масів
-        cell[i].addEventListener('click', function(){ //клікаємо мо якомусь елементу в массиві
-
-                     //e.preventDefault();
-                    if(curClass === 'x' ){ // курва(праильно чи ні кур = curent
-                       curClass = 'o'; // міняєм класс на "о"
-                        console.log('ok');
-                        event.target.classList.add(curClass); //курва клас поміняний на о
-                    //    перевіряємо
-                        console.log(curClass);
-                    } else {
-                        curClass = 'x'; // міша зливай воду  міняїм на "х"
-                        event.target.classList.add(curClass);// курва класс помінявся на "х"
-                        //перевірка курва классу                  }
-                        console.log(curClass);
-                    }
+        cell[i].addEventListener('click', function(e){ //клікаємо мо якомусь елементу в массиві
+                         e.preventDefault();
+                        if(nextClass === 'x'){ // курва(праильно чи ні кур = curent
+                            console.log(cell[0].classList.contains(nextClass).toString() === 'cell x');
+                            if(cell[i].classList.contains(nextClass).toString() === 'cell x' && cell[i].classList.contains(nextClass).toString() ==='cell o'){
+                                console.log('no add');
+                            } else{
+                               nextClass = 'o'; // міняєм класс на "о"
+                                console.log('ok');
+                                event.target.classList.add(nextClass); //курва клас поміняний на о
+                            //    перевіряємо
+                                console.log(nextClass);
+                            }
+                        } else {
+                            nextClass = 'x'; // міша зливай воду  міняїм на "х"
+                            event.target.classList.add(nextClass);// курва класс помінявся на "х"
+                            //перевірка курва классу                  }
+                            console.log(nextClass);
+                        }
 
             });
 
     }
     /*
 
-     var curClass = 'x';
+     var nextClass = 'x';
 
 
      ..где-то в слушателе... (предполагаем что el  - это ячейка в которую надо добавить)
-     el.classList.add(curClass);
-     if (curClass === 'x') { curClass = 'o' } else { curClass='x' }
+     el.classList.add(nextClass);
+     if (nextClass === 'x') { nextClass = 'o' } else { nextClass='x' }
      addEventListener('click', function () {
      var el = event.target;
 
-     if (curClass === 'x') {
-     curClass = 'o'
+     if (nextClass === 'x') {
+     nextClass = 'o'
      } else {
-     curClass='x' }
-     el.classList.add(curClass);
+     nextClass='x' }
+     el.classList.add(nextClass);
      });
-     var curClass = 'x';
+     var nextClass = 'x';
     */
 });
