e2c6ce76c45491a0f8183392ebe88cbe335c24e4
kaflan <kaf_90@mail.ru>
2015-05-26 00:27:44 +0300

    lint err

--- index.js
+++ index.js
@@ -1,39 +1,39 @@
 /* global getWinner */
 window.addEventListener('load', function load() {
   'use strict';
-  var cell = document.querySelectorAll('.cell'); // у массіві всі наші класси целл
+  var cellAll = document.querySelectorAll('.cell'); // у массіві всі наші класси целл
+  var cell = document.querySelector('.cell');
   var nextClass;// хранить класс правильний по кліку фраєра змінюєця поточний клас сім матерів бабі в цицьку
   var winnerEl = document.querySelector('.winner-message');
   var i;
+  var winner;
   var startNewGame = document.querySelector('.startNewGame');// нова гра
   startNewGame.addEventListener('click', function clickNewGame(e) {// start to new game click
     e.preventDefault();
-    for (i = 0; i < cell.length; i++) {
-      cell[i].classList.remove('o');
-      cell[i].classList.remove('x');
+    for (i = 0; i < cellAll.length; i++) {
+      cellAll[i].classList.remove('o');
+      cellAll[i].classList.remove('x');
       nextClass = '';
       winnerEl.innerHTML = '';
     }
   });
-  for (i = 0; i < cell.length; i++) {
-    cell[i].addEventListener('click', function clickEvent(event) {
-      event.preventDefault();
-      if (event.target.classList.contains('x') || event.target.classList.contains('o')) {
-        return;
-      }
-      if (nextClass === 'x') {
-        nextClass = 'o';
-      } else {
-        // міняєм класс на "о"
-        nextClass = 'x'; // міша зливай воду  міняїм на "х"
-      }
-      event.target.classList.add(nextClass);// курва класс помінявся на "х"
-      var winner = getWinner();
-      if (winner === 'x') {
-        winnerEl.innerHTML = 'Крестик победил';
-      } else if (winner === 'o') {
-        winnerEl.innerHTML = 'Нолик победил';
-      }
-    });
-  }
+  cell.addEventListener('click', function clickEvent(event) {
+    event.preventDefault();
+    if (event.target.classList.contains('x') || event.target.classList.contains('o')) {
+      return;
+    }
+    if (nextClass === 'x') {
+      nextClass = 'o';
+    } else {
+      // міняєм класс на "о"
+      nextClass = 'x'; // міша зливай воду  міняїм на "х"
+    }
+    event.target.classList.add(nextClass);// курва класс помінявся на "х"
+    winner = getWinner();
+    if (winner === 'x') {
+      winnerEl.innerHTML = 'Крестик победил';
+    } else if (winner === 'o') {
+      winnerEl.innerHTML = 'Нолик победил';
+    }
+  });
 });
