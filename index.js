d2cfc7215ce16424c9965196b1bc9450547d15e2
kaflan <kaf_90@mail.ru>
2015-05-26 11:07:57 +0300

    94%

--- index.js
+++ index.js
@@ -1,42 +1,44 @@
-window.addEventListener('load', function () {
-    console.log('load complete');
-    var cell = document.querySelectorAll('.cell'), // у массіві всі наші класси целл
-        nextClass,// хранить класс правильний по кліку фраєра змінюєця поточний клас сім матерів бабі в цицьку
-        winnerEl = document.querySelector('.winner-message'),
-        startNewGame = document.querySelector('.startNewGame');// нова гра
-    startNewGame.addEventListener('click', function (e) {// start to new game click
-        e.preventDefault();
-        for (var i = 0; i < cell.length; i++) {
-            cell[i].classList.remove('o'); //delete class 'o' and 'x'
-            cell[i].classList.remove('x');
-            console.log('finish');
-            nextClass = '';
-            winnerEl.innerHTML = '';
-        }
-    });
-    for (var i = 0; i < cell.length; i++) { //шурує масів
-        cell[i].addEventListener('click', function (event) { //клікаємо мо якомусь елементу в массиві
-            event.preventDefault();
-            if (event.target.classList.contains('x') || event.target.classList.contains('o')) {
-                return;
-            }
-                if (nextClass === 'x' ) {
-                    nextClass = 'o';
-                    console.log('ok');
-                    console.log(event.target.classList.contains(nextClass));
-                } else {
-                    // міняєм класс на "о"
-                    console.log(nextClass);
-                    nextClass = 'x'; // міша зливай воду  міняїм на "х"
-                    //перевірка курва классу                  }
-                }
-                    event.target.classList.add(nextClass);// курва класс помінявся на "х"
-            var winner = getWinner();
-            if (winner === 'x') {
-                winnerEl.innerHTML = 'Крестик победил';
-            } else if (winner === 'o') {
-                winnerEl.innerHTML = "Нолик победил";
-            }
-        });
+/* global getWinner */
+window.addEventListener('load', function load() {
+  'use strict';
+  var cellAll = document.querySelectorAll('.cell'); // у массіві всі наші класси целл
+  var cell = document.querySelector('.field');
+  var nextClass;// хранить класс правильний по кліку фраєра змінюєця поточний клас сім матерів бабі в цицьку
+  var winnerEl = document.querySelector('.winner-message');
+  var i;
+  var winner;
+  var startNewGame = document.querySelector('.startNewGame');
+
+  function clickEvent(event) {
+    event.preventDefault();
+    if ((event.target.classList.contains('x') || event.target.classList.contains('o')) || !event.target.classList.contains('cell') && event.target.classList.contains('field')) {
+      return;
     }
+    if (nextClass === 'x') {
+      nextClass = 'o';
+    } else {
+      // міняєм класс на "о"
+      nextClass = 'x'; // міша зливай воду  міняїм на "х"
+    }
+    event.target.classList.add(nextClass);
+    winner = getWinner();
+    if (winner === 'x') {
+      winnerEl.innerHTML = 'Крестик победил';
+      cell.removeEventListener('click', clickEvent);
+    } else if (winner === 'o') {
+      winnerEl.innerHTML = 'Нолик победил';
+      cell.removeEventListener('click', clickEvent);
+    }
+  }
+  function clickNewGame(e) {// start to new game click
+    e.preventDefault();
+    for (i = 0; i < cellAll.length; i++) {
+      cellAll[i].classList.remove('o');
+      cellAll[i].classList.remove('x');
+      nextClass = '';
+      winnerEl.innerHTML = '';
+      cell.addEventListener('click', clickEvent);
+    }
+  }
+  startNewGame.addEventListener('click', clickNewGame);
 });
